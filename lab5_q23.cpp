89dfa43028bc18152df7423cc5e2eb363d64f780
manaswinibehera1 <42205762+manaswinibehera1@users.noreply.github.com>
2018-08-30 19:28:28 +0530

    Add files via upload

---
+++ lab5_q23.cpp
@@ -0,0 +1,15 @@
+//use library
+#include<iostream>
+using namespace std;
+//use main function
+int main (){
+// variable declaration
+ int a,b = 1;
+//ask the user for input
+ cout << "Enter the limit : " << endl;
+ cin >> a;
+ while (b < a){
+ a--;
+ cout << a << endl;
+ }
+} 
