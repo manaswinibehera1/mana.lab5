97cc7dbf92adc8713fb8c67ded9c9ffe245bbb8f
manaswinibehera1 <42205762+manaswinibehera1@users.noreply.github.com>
2018-08-31 01:36:28 +0530

    Add files via upload

--- lab5_q2.cpp
+++ lab5_q2.cpp
@@ -1,21 +1,20 @@
-//use library
+//using library
 #include<iostream>
 using namespace std;
-//use main function
+//using main function
 int main (){
-//declaration
- float a,b,c;
-//ask the user for input
+//declaring variables
+ int a,b,c;
  cout << "Enter three numbers : " << endl;
  cin >> a >> b >> c;
 //condition
- if ((a > b) && (a > b)){
-   cout << a << "is the maximum number" << endl;
+ if ((a > b) && (a > c)){
+ cout << a << "is the maximum number." << endl;
  }
  else if ((b > a) && (b > c)){
-   cout << b << "is the maximum number." << endl;
+ cout << b << "is the maximum number." << endl;
  }
  else {
-   cout << c << "is the maximum number." << endl;
-   }
-}
+ cout << c << "is the maximum number." << endl;
+ }
+}  
---
+++ lab5_q27.cpp
@@ -0,0 +1,15 @@
+//using library
+#include<iostream>
+using namespace std;
+//using main function
+int main (){
+//declaring variables
+ int a = 1,b,c;
+//ask the user for input
+ cout << "Enter the limit : " << endl;
+ cin >> b;
+//calculation
+ c = (b * (b + 1)) / 2;
+//print the result 
+ cout << c << endl;
+}
