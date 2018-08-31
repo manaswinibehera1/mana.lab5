b4b1e00e40a257ae7916a964eb81676c6e12fe1e
manaswinibehera1 <42205762+manaswinibehera1@users.noreply.github.com>
2018-08-31 11:17:17 +0530

    Add files via upload

---
+++ lab5_q24.cpp
@@ -0,0 +1,14 @@
+//using library
+#include<iostream>
+using namespace std;
+//use main function
+int main (){
+ //declaring variables
+ int i = 97;
+ while (i <= 122){
+ char a = char (i);
+ cout << a << endl;
+ i++;
+ }
+}
+ 
